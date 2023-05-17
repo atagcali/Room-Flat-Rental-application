f6e8777c27215f1a616bc638a958f315b0306b32
Alp Tuğrul Ağçalı <tugrul.agcali@ug.bilkent.edu.tr>
2023-05-17 19:21:34 +0300

    backend test
    1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -19,11 +19,13 @@ public class DemoApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         String sql = "INSERT INTO \"user\" (name, email) VALUES ('alpbaba', 'nam@codejava.net')";
-
+        String sql1 = "SELECT name FROM \"user\" WHERE user_id = 1;\n";
         int rows = jdbcTemplate.update(sql);
         if (rows > 0) {
             System.out.println("A new row has been inserted.");
         }
+        String name = jdbcTemplate.queryForObject(sql1, String.class);
+        System.out.println("Name: " + name);
     }
 
 }
